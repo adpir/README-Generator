eedbcea75491e92cb5df8bdf25d928dc736cb338
Alice Piar <adpir30@gmail.com>
2020-11-29 22:59:33 -0600

    new folder

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const inquirer= require('inquirer');
+
+inquirer
+.prompt ([
+ {
+     type:"input",
+     name:"Application",
+     message:"What application you are using?"
+ }
+ 
+
+
+
+])
+.then(response => {
+ console.log('Good Answers', response);
+
+
+
+})
+.catch(error => {
+    console.log('Try again',error);
+
+
+
+
+})
